8d64b2f22251444ff4d1e1304b43ea28f780fc13
Adam <adam.sanderson@theaccessgroup.com>
2015-09-24 20:17:24 +0100

    Add list item component to app

--- src/components/JsDeignTestApp.js
+++ src/components/JsDeignTestApp.js
@@ -1,23 +1,48 @@
 'use strict';
 
-var React = require('react/addons');
-var ReactTransitionGroup = React.addons.TransitionGroup;
+let React = require('react/addons');
+let ReactTransitionGroup = React.addons.TransitionGroup;
+
+let getRepoList = require('components/repoList/getRepoList');
+let ListItemComponent = require('components/repolist/ListItemComponent');
 
 // CSS
 require('normalize.css');
 require('../styles/main.css');
 
-var imageURL = require('../images/yeoman.png');
+let JsDeignTestApp = React.createClass({
+    displayName: 'JsDesignTestApp',
+    getInitialState: function () {
+        return {
+            repos: []
+        };
+    },
+    componentDidMount: function() {
+        let self = this;
+        getRepoList().then((repos) => {
+            self.setState({
+                repos: repos
+            });
+        });
+    },
+    render: function() {
+        let self = this;
 
-var JsDeignTestApp = React.createClass({
-  render: function() {
-    return (
-      <div className="main">
-        <ReactTransitionGroup transitionName="fade">
-          <img src={imageURL} />
-        </ReactTransitionGroup>
-      </div>
-    );
+        let repos = self.state.repos.map(function(item) {
+            const ListItemComponentProps = {
+                key: item.id,
+                item: item
+            };
+            return <ListItemComponent {...ListItemComponentProps} />;
+        });
+
+        return (
+            <div className="main">
+                <ReactTransitionGroup transitionName="fade">
+                    {repos}
+                </ReactTransitionGroup>
+            </div>
+        );
   }
 });
 React.render(<JsDeignTestApp />, document.getElementById('content')); // jshint ignore:line
--- test/spec/components/JsDeignTestApp.spec.js
+++ test/spec/components/JsDeignTestApp.spec.js
@@ -1,19 +1,19 @@
 'use strict';
 
-describe('JsDeignTestApp', () => {
+describe('JsDesignTestApp', () => {
   let React = require('react/addons');
-  let JsDeignTestApp, component;
+  let JsDesignTestApp, component;
 
   beforeEach(() => {
     let container = document.createElement('div');
     container.id = 'content';
     document.body.appendChild(container);
 
-    JsDeignTestApp = require('components/JsDeignTestApp.js');
-    component = React.createElement(JsDeignTestApp);
+    JsDesignTestApp = require('components/JsDeignTestApp.js');
+    component = React.createElement(JsDesignTestApp);
   });
 
-  it('should create a new instance of JsDeignTestApp', () => {
-    expect(component).toBeDefined();
+  it('should create a new instance of JsDesignTestApp', () => {
+    expect(component).to.exist;
   });
 });
