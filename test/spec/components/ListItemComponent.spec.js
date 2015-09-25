c166d2af76c4af10d43a01031ab3155ecef125ee
Adam <adam.sanderson@theaccessgroup.com>
2015-09-25 23:34:36 +0100

    Add initial component tests

--- test/spec/components/ListItemComponent.spec.js
+++ test/spec/components/ListItemComponent.spec.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Uncomment the following lines to use the react test utilities
-// import React from 'react/addons';
-// const TestUtils = React.addons.TestUtils;
+import React from 'react/addons';
+const TestUtils = React.addons.TestUtils;
 
 import createComponent from 'helpers/createComponent';
 import ListItemComponent from 'components/ListItemComponent.js';
@@ -10,11 +10,25 @@ import ListItemComponent from 'components/ListItemComponent.js';
 describe('ListItemComponent', () => {
     let ListItemComponentComponent;
 
-    beforeEach(() => {
-        ListItemComponentComponent = createComponent(ListItemComponent);
-    });
+    describe('default values', function () {
+        beforeEach(() => {
+            let listItemProps = {
+                item: {
+                    name: '',
+                    stargazers_count: 1,
+                    forks_count: 2
+                }
+            };
+            ListItemComponentComponent = createComponent(ListItemComponent, listItemProps);
+        });
 
-    it('should have its component name as default className', () => {
-        // expect(ListItemComponentComponent._store.props.className).toBe('ListItemComponent');
+        it('expect a list item to be created', function () {
+            expect(ListItemComponentComponent.type).to.equal('li');
+        });
+        it('exect the default list to have 1 p child and 1 null child', function () {
+            expect(ListItemComponentComponent.props.children).to.have.length(2);
+            expect(ListItemComponentComponent.props.children[0].type).to.be.equal('p');
+            expect(ListItemComponentComponent.props.children[1]).to.be.null;
+        });
     });
 });
